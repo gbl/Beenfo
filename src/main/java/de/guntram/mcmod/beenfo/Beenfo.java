8e07d5e87fb0d3971c23020020964ddf9ef00a61
Leandro Pardini <leandrotlz@gmail.com>
2020-01-11 10:03:37 -0300

    Add bee names to the tooltip. Ensure the tooltip appears before the advanced tooltip info. Color the honey level and bee count gold.

--- src/main/java/de/guntram/mcmod/beenfo/Beenfo.java
+++ src/main/java/de/guntram/mcmod/beenfo/Beenfo.java
@@ -7,7 +7,7 @@ public class Beenfo implements ClientModInitializer
 {
     public static final String MODID = "beenfo";
     public static final String MODNAME = "Beenfo";
-    public static final String VERSION = "1.15-fabric0.4.23-1.0.1";
+    public static final String VERSION = "1.15-fabric0.4.23-1.0.2";
 
     @Override
     public void onInitializeClient() {
