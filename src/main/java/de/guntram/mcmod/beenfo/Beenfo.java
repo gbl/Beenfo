08f2cfc459c44db7f89f25fc794a8a0199da9fe9
Guntram Blohm <gbl@guntram.de>
2020-07-01 07:57:39 +0200

    Forge 1.15 version

--- src/main/java/de/guntram/mcmod/beenfo/Beenfo.java
+++ src/main/java/de/guntram/mcmod/beenfo/Beenfo.java
@@ -1,15 +1,25 @@
 package de.guntram.mcmod.beenfo;
 
-import net.fabricmc.api.ClientModInitializer;
+import de.guntram.mcmod.beenfo.mixin.TooltipMixin;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-
-public class Beenfo implements ClientModInitializer
+@Mod("beenfo")
+public class Beenfo
 {
     public static final String MODID = "beenfo";
     public static final String MODNAME = "Beenfo";
     public static final String VERSION = "1.15-fabric0.4.23-1.0.3";
 
-    @Override
-    public void onInitializeClient() {
+    public Beenfo() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::init);        
+    }
+    
+    public void init(final FMLCommonSetupEvent event) {
+        MinecraftForge.EVENT_BUS.register(new TooltipMixin());
     }
 }
--- src/main/java/de/guntram/mcmod/beenfo/mixin/TooltipMixin.java
+++ src/main/java/de/guntram/mcmod/beenfo/mixin/TooltipMixin.java
@@ -1,72 +1,65 @@
 package de.guntram.mcmod.beenfo.mixin;
 
 import java.util.List;
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-@Mixin(ItemStack.class)
-public abstract class TooltipMixin {
-    
-    @Shadow public abstract boolean isEmpty();
-    @Shadow public abstract Item getItem();
-    @Shadow public abstract CompoundTag getTag();
-    
-//    @Inject(method="getTooltip(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/client/util/ITooltipFlag;)Ljava/util/List",
-    @Inject(method="getTooltip",            
-            at=@At("RETURN"), locals=LocalCapture.CAPTURE_FAILHARD)
-    private void getTooltipdone(PlayerEntity playerIn, TooltipContext advanced, 
-            CallbackInfoReturnable<List> ci,
-            List<Text> list) {
 
+public class TooltipMixin {
+    
+   @SubscribeEvent
+    public void getTooltipdone(final ItemTooltipEvent event) {
+        ItemStack stack = event.getItemStack();
+        List<ITextComponent> list = event.getToolTip();
         try {
-            if (!this.isEmpty() && (this.getItem() == Items.BEE_HIVE || this.getItem() == Items.BEE_NEST)) {
-                CompoundTag tag = this.getTag();
-                if (tag != null) {
-                    String honeyLevel = tag.getCompound("BlockStateTag").getString("honey_level");  // wtf this is a string ???
-                    if (honeyLevel == null || honeyLevel.isEmpty()) {
-                        honeyLevel = "0";
-                    }
-
-                    ListTag bees = tag.getCompound("BlockEntityTag").getList("Bees", 10);
-                    int beeCount = bees.size();
-
-                    // Insert our lines in reverse order and always at the beginning of the list,
-                    // this way they will appear before the advanced tooltips if enabled.
-                    for (int i = 0; i < beeCount; i++)
-                    {
-                        tag = bees.getCompound(i).getCompound("EntityData");
-                        if (tag != null && tag.contains("CustomName", 8))
-                        {
-                            String beeName = tag.getString("CustomName");
-                            list.add(Math.min(1, list.size()), new TranslatableText("tooltip.name", Text.Serializer.fromJson(beeName).getString()));
-                        }
-                    }
-
-                    list.add(Math.min(1, list.size()), new TranslatableText("tooltip.bees", beeCount));
-                    list.add(Math.min(1, list.size()), new TranslatableText("tooltip.honey", honeyLevel));
+            if (!stack.isEmpty()) {
+                CompoundNBT tag = stack.getTag();
+                if (tag == null)
+                    return;
+                CompoundNBT bsTag = tag.getCompound("BlockStateTag");
+                if (bsTag == null || !bsTag.contains("honey_level"))
+                    return;
+                CompoundNBT beTag = tag.getCompound("BlockEntityTag");
+                if (beTag == null || !beTag.contains("Bees"))
+                    return;
+                
+                String honeyLevel = bsTag.getString("honey_level");  // wtf this is a string ???
+                if (honeyLevel == null || honeyLevel.isEmpty()) {
+                    honeyLevel = "0";
                 }
+
+                ListNBT bees = beTag.getList("Bees", 10);
+                int beeCount = bees.size();
+
+                // Insert our lines in reverse order and always at the beginning of the list,
+                // this way they will appear before the advanced tooltips if enabled.
+                for (int i = 0; i < beeCount; i++)
+                {
+                    tag = bees.getCompound(i).getCompound("EntityData");
+                    if (tag != null && tag.contains("CustomName", 8))
+                    {
+                        String beeName = tag.getString("CustomName");
+                        list.add(Math.min(1, list.size()), new TranslationTextComponent("tooltip.name", ITextComponent.Serializer.fromJson(beeName).getString()));
+                    }
+                }
+
+                list.add(Math.min(1, list.size()), new TranslationTextComponent("tooltip.bees", beeCount));
+                list.add(Math.min(1, list.size()), new TranslationTextComponent("tooltip.honey", honeyLevel));
             }
         } catch (NullPointerException ex) {
             System.out.println("NPE in getTooltipdone");
             try {
-                Item item = this.getItem();
+                Item item = stack.getItem();
                 if (item == null) {
                     System.out.println("item is null");
                 } else {
-                    System.out.println("item is "+this.getItem().getTranslationKey());
+                    System.out.println("item is "+stack.getItem().getTranslationKey());
                 }
             } catch (NullPointerException ex2) {
 
