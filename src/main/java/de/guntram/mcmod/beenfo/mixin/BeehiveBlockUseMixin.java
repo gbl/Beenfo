9fd8b253877be2566ef97d466561331e9e3bf2ed
Guntram Blohm <gbl@guntram.de>
2020-07-13 07:23:29 +0200

    Server part: Any item that isn't a block will open the GUI, but only at RETURN when super.onUse() gets called, and only if the item in hand isn't a block, so shearing/bottling/placing blocks won't.

--- src/main/java/de/guntram/mcmod/beenfo/mixin/BeehiveBlockUseMixin.java
+++ src/main/java/de/guntram/mcmod/beenfo/mixin/BeehiveBlockUseMixin.java
@@ -8,11 +8,13 @@ package de.guntram.mcmod.beenfo.mixin;
 import de.guntram.mcmod.beenfo.BeenfoServer;
 import net.minecraft.block.BeehiveBlock;
 import static net.minecraft.block.BeehiveBlock.HONEY_LEVEL;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BeehiveBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Items;
+import net.minecraft.item.Item;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -31,26 +33,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BeehiveBlock.class)     
 public class BeehiveBlockUseMixin {
     
-    @Inject(method="onUse", at=@At("HEAD"))
+    @Inject(method="onUse", at=@At(value="RETURN", ordinal=1))
     public void onUseStick(BlockState blockState, World world, BlockPos blockPos,
             PlayerEntity playerEntity, Hand hand, BlockHitResult blockHitResult,
             CallbackInfoReturnable ci) {
-    
-        // Yes, this is Hand.MAIN_HAND, not the `hand` parameter. A non-tool
-        // in the main hand means we'll be called with hand=OFF_HAND, but we
-        // want the main hand to be empty to do our thing.
-        if (!world.isClient() && playerEntity.getStackInHand(Hand.MAIN_HAND).getItem() == Items.AIR) {
-            int honey = blockState.get(HONEY_LEVEL);
-            int bees = 0;
-            ListTag tag = null;
-            BlockEntity entity = world.getBlockEntity(blockPos);
-            if (entity instanceof BeehiveBlockEntity) {
-                BeehiveBlockEntity bbe = (BeehiveBlockEntity) entity;
-                tag = bbe.getBees();
-                bees = tag.size();
+
+        // As we're injecting into the point where super.onUse() gets called,
+        // shears and bottles do not reach this point.
+        if (!world.isClient()) {
+            Item item = playerEntity.getStackInHand(hand).getItem();
+            Block block = Block.getBlockFromItem(item);
+            // Any item that isn't a block will return Blocks.AIR here
+            if (block == Blocks.AIR) {
+                int honey = blockState.get(HONEY_LEVEL);
+                ListTag tag = null;
+                BlockEntity entity = world.getBlockEntity(blockPos);
+                if (entity instanceof BeehiveBlockEntity) {
+                    BeehiveBlockEntity bbe = (BeehiveBlockEntity) entity;
+                    tag = bbe.getBees();
+                }
+                // System.out.println(honey + " honey, "+bees+" bees"+", item="+playerEntity.getStackInHand(hand).getItem());
+                BeenfoServer.sendBeehiveInfo(playerEntity, honey, tag);
             }
-            System.out.println(honey + " honey, "+bees+" bees"+", item="+playerEntity.getStackInHand(hand).getItem());
-            BeenfoServer.sendBeehiveInfo(playerEntity, honey, tag);
         }
     }
 }
